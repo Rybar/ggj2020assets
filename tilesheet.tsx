09a3898807b10374c93d67bac14bca67a52bbaf0
rybar <ryan.malm@gmail.com>
2020-02-01 09:59:01 -0600

    planet surface prototype

---
+++ tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.12.11" name="tilesheet" tilewidth="8" tileheight="8" tilecount="896" columns="16">
+ <image source="tilesheet.png" width="128" height="448"/>
+</tileset>
